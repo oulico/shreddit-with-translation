d42de01f4525354b7f054c55f98d628a1952233c
JH <13332150+oulico@users.noreply.github.com>
2024-12-06 14:21:43 +0900

    FIX: hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,94 +1,96 @@
-import { type Metadata } from "next";
-import { Inter } from "next/font/google";
+import {type Metadata} from "next";
+import {Inter} from "next/font/google";
 
 import {
-  Analytics,
-  Navbar,
-  Providers,
-  TailwindIndicator,
-  ThemeProvider,
+    Analytics,
+    Navbar,
+    Providers,
+    TailwindIndicator,
+    ThemeProvider,
 } from "~/components";
-import { Toaster } from "~/components/ui/toaster";
-import { siteConfig } from "~/config/site";
-import { cn } from "~/lib/utils";
+import {Toaster} from "~/components/ui/toaster";
+import {siteConfig} from "~/config/site";
+import {cn} from "~/lib/utils";
 
 import "~/styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 interface RootLayoutProps {
-  authModal: React.ReactNode;
-  children: React.ReactNode;
+    authModal: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  creator: siteConfig.creator,
-  authors: siteConfig.authors,
-  keywords: siteConfig.keywords,
-  icons: {
-    icon: "/favicon.ico",
-  },
-  // manifest: `${siteConfig.url}/site.webmanifest`,
-  openGraph: {
-    title: siteConfig.name,
+    title: {
+        default: siteConfig.name,
+        template: `%s | ${siteConfig.name}`,
+    },
     description: siteConfig.description,
-    siteName: siteConfig.name,
-    url: siteConfig.url,
-    type: "website",
-    locale: "en_US",
-    // images: [
-    //   {
-    //     url: siteConfig.ogImage,
-    //     width: 1200,
-    //     height: 630,
-    //     alt: siteConfig.name,
-    //   },
-    // ],
-  },
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    // images: [`${siteConfig.url}/og.jpg`],
-    // creator: "@example",
-  },
-  metadataBase: new URL(siteConfig.url),
+    creator: siteConfig.creator,
+    authors: siteConfig.authors,
+    keywords: siteConfig.keywords,
+    icons: {
+        icon: "/favicon.ico",
+    },
+    // manifest: `${siteConfig.url}/site.webmanifest`,
+    openGraph: {
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name,
+        url: siteConfig.url,
+        type: "website",
+        locale: "en_US",
+        // images: [
+        //   {
+        //     url: siteConfig.ogImage,
+        //     width: 1200,
+        //     height: 630,
+        //     alt: siteConfig.name,
+        //   },
+        // ],
+    },
+    themeColor: [
+        {media: "(prefers-color-scheme: light)", color: "white"},
+        {media: "(prefers-color-scheme: dark)", color: "black"},
+    ],
+    twitter: {
+        card: "summary_large_image",
+        title: siteConfig.name,
+        description: siteConfig.description,
+        // images: [`${siteConfig.url}/og.jpg`],
+        // creator: "@example",
+    },
+    metadataBase: new URL(siteConfig.url),
 };
 
-export default function RootLayout({ authModal, children }: RootLayoutProps) {
-  return (
-    <html lang="en">
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background pt-12 antialiased",
-          inter.className,
-        )}
-      >
+export default function RootLayout({authModal, children}: RootLayoutProps) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <head/>
+        <body
+            className={cn(
+                "min-h-screen bg-background pt-12 antialiased",
+                inter.className,
+            )}
+        >
         <Providers>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <Navbar />
-            {authModal}
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                <div className="contents" suppressHydrationWarning>
+                    <Navbar/>
+                    {authModal}
 
-            <div className="container mx-auto h-full max-w-7xl pt-12">
-              {children}
-            </div>
+                    <div className="container mx-auto h-full max-w-7xl pt-12">
+                        {children}
+                    </div>
 
-            <Analytics />
-            <TailwindIndicator />
-            <Toaster />
-          </ThemeProvider>
+                    <Analytics/>
+                    <TailwindIndicator/>
+                    <Toaster/>
+                </div>
+            </ThemeProvider>
         </Providers>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
